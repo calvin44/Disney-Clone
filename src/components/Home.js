3f4d2929efa7ed673b04726cbd020b18a4d096a6
Calvin Fredicson <calvin.fredicson@gmail.com>
2021-07-13 00:54:12 +0800

    add viewer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
-import Viewer from './Viewer'
+import Viewers from './Viewers'
 
 const Home = () => {
     return (
         <Container>
             <ImgSlider />
-            <Viewer />
+            <Viewers />
         </Container>
     )
 }
