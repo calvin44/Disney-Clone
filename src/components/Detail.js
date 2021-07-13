c3fca3b45dbef99f55ea65303073599d7d132ea1
Calvin Fredicson <calvin.fredicson@gmail.com>
2021-07-13 15:22:30 +0800

    add detail page

---
+++ src/components/Detail.js
@@ -0,0 +1,138 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Detail = () => {
+    return (
+        <Container>
+            <Background>
+                <img
+                    src="https://d23.com/app/uploads/2018/05/1180w-600h_052918_bao-short-info-from-pixar-day-780x440.jpg" alt="" />
+            </Background>
+            <ImageTitle>
+                <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1344&aspectRatio=1.78" alt="" />
+            </ImageTitle>
+            <Controls>
+                <PlayButton>
+                    <img src="/images/play-icon-black.png" alt="" />
+                    <span>PLAY</span>
+                </PlayButton>
+                <TrailerButton>
+                    <img src="/images/play-icon-white.png" alt="" />
+                    <span>TRAILER</span>
+                </TrailerButton>
+                <AddButton>
+                    <span>+</span>
+                </AddButton>
+                <GroupWatchButton>
+                    <img src="/images/group-icon.png" alt="" />
+                </GroupWatchButton>
+            </Controls>
+            <SubTitle>
+                2018 - 7m - Family - Fantasy - Kids - Animation
+            </SubTitle>
+            <Description>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            </Description>
+        </Container>
+    )
+}
+
+export default Detail
+
+const Container = styled.div`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vh + 5px);
+    position: relative;
+`
+
+const Background = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
+    opacity: 0.8;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`
+
+const ImageTitle = styled.div`
+    height: 30vh;
+    width: 35vw;
+    min-height: 170px;
+    min-width: 200px;
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+`
+
+const Controls = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const PlayButton = styled.button`
+    border-radius: 4px;
+    font-size: 15px;
+    display: flex;
+    align-items: center;
+    height: 56px;
+    background-color: rgb(249, 249,249);
+    border: none;
+    padding: 0px 24px;
+    margin-right: 22px;
+    letter-spacing: 1.8px;
+    cursor: pointer;
+
+    &:hover {
+        background: rgb(198, 198, 198);
+    }
+`
+const TrailerButton = styled(PlayButton)`
+    background-color: rgba(0,0,0,0.3);
+    borrder: 1px solid rgb(249, 249,249);
+    color: rgb(249, 249,249);
+    text-transform: uppercase;
+`
+const AddButton = styled.button`
+    margin-right: 16px;
+    width: 44px;
+    height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    border: 2px solid white;
+    background-color: rgba(0,0,0,0.6);
+    cursor: pointer;
+
+    span {
+        font-size: 30px;
+        color: white;
+    }
+`
+const GroupWatchButton = styled(AddButton)`
+    background-color: black;
+`
+
+const SubTitle = styled.div`
+    color: rgb(249, 249, 249);
+    font-size: 15px;
+    min-height: 20px;
+    margin-top: 26px;
+`
+
+const Description = styled.div`
+    line-height: 1.4;
+    font-size: 20px;
+    margin-top: 16px;
+    color: rgb(249, 249, 249);
+`
--- src/components/Header.js
+++ src/components/Header.js
@@ -53,7 +53,6 @@ const NavMenu = styled.div`
     flex: 1;
     margin-left: 20px;
     align-items: center;
-    overflow-x: hidden;
     a {
         display: flex;
         align-items: center;
